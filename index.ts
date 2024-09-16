bbad9cc0a857914ca027d50c0f4ce0e01e174123
Matthias <m@tth.es>
2024-09-16 12:18:17 +0200

    sync

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 
 
 export * from "./src/breakpoints";
-export * from "./src/await/body";
 export * from "./src/debouncer";
 export * from "./src/widget-position";
 export * from "./src/create-element";
+
+export * from "./src/await/dom-ready";
+export * from "./src/await/body";
---
+++ src/await/dom-ready.ts
@@ -0,0 +1,18 @@
+
+/**
+ * Wait for DomContentLoaded or resolve immediate
+ *
+ * <example>
+ * await MicxToolsVx.domReady();
+ * </example>
+ *
+ * @return {Promise<string>}
+ */
+export async function ka_dom_ready () : Promise<string> {
+    return new Promise<string>((resolve) => {
+        if (document.readyState === "complete" || document.readyState === "interactive")
+            return resolve("loaded");
+
+        document.addEventListener("DOMContentLoaded", ()=>resolve('DOMContentLoaded'));
+    });
+}
